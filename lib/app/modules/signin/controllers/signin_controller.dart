fffa64d778ee700431b6de2dae2031250c2c8e36
unustudent <nanda.19572011009@students.unugha.id>
2024-01-16 22:28:57 +0700

    edit : sign in page

--- lib/app/modules/signin/controllers/signin_controller.dart
+++ lib/app/modules/signin/controllers/signin_controller.dart
@@ -1,23 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SigninController extends GetxController {
-  //TODO: Implement SigninController
+  final formkey = GlobalKey<FormState>();
+  final visibil = true.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  final emailC = TextEditingController();
+  final passC = TextEditingController();
 }
--- lib/app/modules/signin/views/signin_view.dart
+++ lib/app/modules/signin/views/signin_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:simple_quiz/app/data/constant/colors.dart';
+import 'package:simple_quiz/app/data/core/extentions.dart';
+import 'package:simple_quiz/app/data/widgets/form.dart';
+import 'package:simple_quiz/app/modules/home/views/home_view.dart';
 
 import '../controllers/signin_controller.dart';
 
@@ -9,49 +13,124 @@ class SigninView extends GetView<SigninController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            const Column(
+        child: Center(
+          child: Form(
+            key: controller.formkey,
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20),
               children: [
-                Text('Quiz App'),
-                Text('Selamat Datang'),
-                SizedBox(height: 30),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: AppColors.biruTua,
+                    fontSize: 24.0.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 47),
+                Formulir.formEmail(ctr: controller.emailC),
+                const SizedBox(height: 24),
+                Obx(() => Formulir.formPass(
+                      ctr: controller.passC,
+                      visibil: controller.visibil.value,
+                      onPres: () {
+                        controller.visibil.toggle();
+                      },
+                    )),
+                const SizedBox(height: 14),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Forgot password ?',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontSize: 12.0.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {
+                    print(controller.emailC.text.toString());
+                    if (controller.formkey.currentState!.validate()) {
+                      Get.to(const HomeView());
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.biruTua,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontSize: 18.0.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(height: 58),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account yet? "),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                            color: AppColors.biruTua,
+                            decoration: TextDecoration.underline,
+                            fontSize: 14.0.sp,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(
-                alignLabelWithHint: true,
-                labelText: 'Email',
-                hintText: '....@gmail.com',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (GetUtils.isNullOrBlank(value) == true) {
-                  return 'Kolom wajib diisi';
-                }
-                if (!GetUtils.isEmail(value!)) {
-                  return 'Ini bukan email';
-                }
-                return null;
-              },
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            shape: const RoundedRectangleBorder(),
-            backgroundColor: Colors.lightBlue,
-            foregroundColor: Colors.white),
-        onPressed: () {
-          //
-        },
-        child: const Text('Selanjutnya'),
+    );
+  }
+
+  TextFormField emailForm() {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        alignLabelWithHint: true,
+        labelText: 'Email',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: Colors.grey.shade500,
+          ),
+        ),
+        prefixIcon: Icon(
+          Icons.person_2_rounded,
+          color: Colors.grey.shade500,
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 15),
       ),
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (GetUtils.isNullOrBlank(value) == true) {
+          return 'Kolom wajib diisi';
+        }
+        if (!GetUtils.isEmail(value!)) {
+          return 'Ini bukan email';
+        }
+        return null;
+      },
     );
   }
 }
