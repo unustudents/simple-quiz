81986d4040f2cdef2d65017dc1f9bd4792a9fb03
unustudent <nanda.19572011009@students.unugha.id>
2024-01-16 22:27:31 +0700

    add : color

---
+++ lib/app/data/constant/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color biruTua = const Color(0xFF065EB2);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:simple_quiz/color_schemes.g.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -11,6 +12,10 @@ void main() {
       // initialRoute: AppPages.INITIAL,
       initialRoute: Routes.SIGNIN,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        colorScheme: lightColorScheme,
+        fontFamily: 'Poppins',
+      ),
     ),
   );
 }
