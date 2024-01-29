f80deb561c574f4c909a52e35ad11a876ec97320
unustudent <nanda.19572011009@students.unugha.id>
2024-01-29 22:31:20 +0700

    initial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'color_schemes.g.dart';
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
+      debugShowCheckedModeBanner: false,
       getPages: AppPages.routes,
       theme: ThemeData(
         colorScheme: lightColorScheme,
