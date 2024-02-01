6f6abc6b196c44f459b4f962223c30bce942bfcc
unustudent <nandaakmal40@gmail.com>
2024-02-01 23:39:16 +0700

    edit register page

---
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../data/models/user_model.dart';
+import '../../../data/widgets/snack.dart';
+import '../../../routes/app_pages.dart';
+
+class RegisterController extends GetxController {
+  final formkey = GlobalKey<FormState>();
+  final l = false.obs;
+  final visible = true.obs;
+  final tokenX = false.obs;
+
+  final tokenT = TextEditingController();
+  final emailT = TextEditingController();
+  final paswdT = TextEditingController();
+  final namaT = TextEditingController();
+
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  // stream token
+  Stream<DocumentSnapshot<Map<String, dynamic>>> streamToken() async* {
+    yield* _firestore.collection('token').doc('valid').snapshots();
+  }
+
+  // membuat akun
+  Future<void> onCreateAccount() async {
+    l.value = true;
+    UserModel data =
+        UserModel(email: emailT.text, name: namaT.text, password: paswdT.text);
+    try {
+      final credential = await _auth.createUserWithEmailAndPassword(
+          email: emailT.text, password: paswdT.text);
+      if (credential.user != null) {
+        String uid = credential.user!.uid;
+        await _auth.currentUser!.updateDisplayName(namaT.text);
+        onRegister(uid, data);
+        await _auth.currentUser!.sendEmailVerification();
+        Get.offAllNamed(Routes.SPLASH);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        SnackbarCustom.errorToast(msg: 'Kata sandi terlalu pendek.');
+      } else if (e.code == 'email-already-in-use') {
+        SnackbarCustom.errorToast(
+            msg: 'Anda memiliki akun terdaftar menggunakan email ini.');
+      } else if (e.code == 'invalid-email') {
+        SnackbarCustom.errorToast(msg: 'Email tidak valid.');
+      }
+    } catch (e) {
+      SnackbarCustom.errorToast(msg: 'Tidak dapat membuat akun.');
+    } finally {
+      l.value = false;
+    }
+  }
+
+  // register
+  void onRegister(String uid, UserModel data) async {
+    return _firestore
+        .collection('users')
+        .doc(uid)
+        .set(data.toMap())
+        .then((v) => SnackbarCustom.successToast(msg: 'Berhasil registrasi'))
+        .catchError((e) =>
+            SnackbarCustom.errorToast(msg: 'Tidak dapat registrasi, error $e'));
+  }
+
+  @override
+  void onClose() {
+    Get.delete();
+    super.onClose();
+  }
+}
---
+++ lib/app/modules/register/views/register_view.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../data/core/extentions.dart';
+import '../../../data/widgets/button.dart';
+import '../../../data/widgets/form.dart';
+import '../controllers/register_controller.dart';
+
+class RegisterView extends GetView<RegisterController> {
+  const RegisterView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Registrasi')),
+      body: Center(
+        child: Form(
+          key: controller.formkey,
+          /* menggunakan streambuilder untuk memantau value dari token apakah true atau false, 
+          jika true maka form akan muncul, jika false maka form akan menutup */
+          child: StreamBuilder(
+            stream: controller.streamToken(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Text('No Internet');
+                case ConnectionState.waiting:
+                  return const CircularProgressIndicator();
+                default:
+                  if (snapshot.hasData) {
+                    var isValid = snapshot.data!.data()!['is_valid'];
+                    if (isValid) {
+                      // jika tokennya true / aktif
+                      return ListView(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.all(5.0.wp),
+                        children: [
+                          // formulir - email
+                          Formulir.formEmail(ctr: controller.emailT),
+                          SizedBox(height: 6.0.wp),
+
+                          // formulir - password
+                          Obx(() => Formulir.formPass(
+                                visibil: controller.visible.value,
+                                onPres: () => controller.visible.toggle(),
+                                ctr: controller.paswdT,
+                              )),
+                          SizedBox(height: 6.0.wp),
+
+                          // formulir - nama lengkap
+                          Formulir.formReguler(
+                              title: 'Nama Lengkap', ctr: controller.namaT),
+                          SizedBox(height: 6.0.wp),
+
+                          // formulir - tombol
+                          Obx(() => buttonBlueObx(
+                                onPressed: () {
+                                  if (controller.formkey.currentState!
+                                      .validate()) controller.onCreateAccount();
+                                },
+                                l: controller.l.value,
+                                teks: 'Register',
+                              ))
+                        ],
+                      );
+                    } else {
+                      // jika tokenya false / tidak aktif
+                      return Padding(
+                        padding: EdgeInsets.all(5.0.wp),
+                        child: Text(
+                          'Mohon maaf sesi registrasi sudah di tutup, silahkan hubungi panitia 😎👌',
+                          style: TextStyle(
+                              fontSize: 14.0.sp, fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    }
+                  } else {
+                    // jika token tidak ada di database
+                    return Padding(
+                      padding: EdgeInsets.all(5.0.wp),
+                      child: Text(
+                        'Fitur disable ! 😩',
+                        style: TextStyle(
+                            fontSize: 14.0.sp, fontWeight: FontWeight.w500),
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  }
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
