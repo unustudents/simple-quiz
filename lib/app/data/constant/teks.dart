9400656f77d32beef42c48d65daebf652cda0d1c
unustudent <nanda.19572011009@students.unugha.id>
2024-02-10 20:01:23 +0700

    add teks ✍

---
+++ lib/app/data/constant/teks.dart
@@ -0,0 +1,9 @@
+class AppTexts {
+  static String logo = 'assets/images/logo.png';
+  static String logoPMII = 'assets/images/logo_pmii.png';
+  static String iconProfil = 'assets/images/profile.png';
+  static String iconInfo = 'assets/images/info.png';
+  static String iconConfirm = 'assets/images/confirm.png';
+  static String splash = 'assets/images/splash.png';
+  static String background = 'assets/images/background.jpg';
+}
