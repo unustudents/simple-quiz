9c6e86878892b503274e55633849cbbba4294818
unustudent <nandaakmal40@gmail.com>
2024-02-11 12:36:24 +0700

    add form bottom sheet

--- lib/app/data/widgets/card.dart
+++ lib/app/data/widgets/card.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import '../core/extentions.dart';
 import '../constant/colors.dart';
 
 class CardCustom {
+  static Card formBottomSheet({Key? key, required List<Widget> children}) {
+    return Card(
+      child: Column(
+        children: [
+          FractionallySizedBox(
+            widthFactor: 0.25,
+            child: Container(
+              height: 5.0,
+              decoration: const BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.all(Radius.circular(2.5)),
+              ),
+              margin: const EdgeInsets.symmetric(vertical: 12.0),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Form(
+              key: key,
+              child: ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.fromLTRB(4.0.wp, 0, 4.0.wp, 6.0.wp),
+                physics: const PageScrollPhysics(),
+                children: children,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   static Card cardOption(
       {required String title,
       required String teks,
