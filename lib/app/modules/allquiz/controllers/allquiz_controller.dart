0b8fc2e62496a35fa6fe7b45a296f64fa81cda0f
unustudent <nanda.19572011009@students.unugha.id>
2024-02-16 22:17:05 +0700

    change add quiz

--- lib/app/modules/allquiz/controllers/allquiz_controller.dart
+++ lib/app/modules/allquiz/controllers/allquiz_controller.dart
@@ -1,29 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
+import '../../../data/models/models.dart';
+import '../../../data/widgets/snack.dart';
+
 class AllquizController extends GetxController {
   final l = false.obs;
-
   final _firestore = FirebaseFirestore.instance.collection('all_quiz');
 
   Stream<QuerySnapshot<Map<String, dynamic>>> fetchAllQuiz(String uid) async* {
     yield* _firestore.where('category', isEqualTo: uid).snapshots();
   }
 
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
-
-  // @override
-  // void onReady() {
-  //   super.onReady();
-  // }
-
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
-
-  // void increment() => count.value++;
+  void createQuiz(
+      {required String title,
+      String? description,
+      required String category}) async {
+    l.value = true;
+    CreateQuiz data =
+        CreateQuiz(title: title, description: description, category: category);
+    await _firestore.add(data.toMap()).then((v) {
+      Get.back();
+      SnackbarCustom.successToast(msg: 'Selamat 🥳 berhasil membuat kuis');
+    }).catchError((e) {
+      Get.back();
+      SnackbarCustom.errorToast(msg: 'Maaf 🙏 gagal membuat kuis');
+    });
+    l.value = false;
+  }
 }
--- lib/app/modules/allquiz/views/allquiz_view.dart
+++ lib/app/modules/allquiz/views/allquiz_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shimmer/shimmer.dart';
 
+import '../../../data/constant/colors.dart';
 import '../../../data/core/extentions.dart';
 import '../../../data/widgets/button.dart';
 import '../../../data/widgets/card.dart';
@@ -13,106 +14,99 @@ class AllquizView extends GetView<AllquizController> {
   const AllquizView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    // variabel
     Map<String, dynamic>? data = Get.arguments;
-    return Scaffold(
-        appBar: AppBar(title: Text('Semua Kuis - ${data?["title"]}'), actions: [
-          IconButton(
-              onPressed: () =>
-                  Get.toNamed(Routes.ADDQUIZ, arguments: {'uid': data?['uid']}),
-              icon: const Icon(Icons.note_add_rounded))
-        ]),
-        body: Padding(
-          padding: EdgeInsets.all(3.0.wp),
-          child: StreamBuilder(
-              stream: controller.fetchAllQuiz("${data?['uid']}"),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                    return const Center(child: Text('No Internet'));
-                  case ConnectionState.waiting:
-                    return ListView.separated(
-                      itemBuilder: (BuildContext context, int index) {
-                        return Shimmer.fromColors(
-                          baseColor: Colors.grey.shade200,
-                          highlightColor: Colors.cyan.shade300,
-                          child: const Card(child: SizedBox(height: 80)),
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) =>
-                          const SizedBox(height: 10),
-                      itemCount: 3,
-                    );
-                  default:
-                    if (snapshot.hasData) {
-                      return ListView.separated(
-                        itemBuilder: (context, index) {
-                          var sample = snapshot.data!.docs.elementAt(index);
-
-                          return CardCustom.cardQuiz(
-                              onTap: () => Get.toNamed(Routes.PRAQUIZ,
-                                  arguments: sample.id),
-                              index: index,
-                              title: sample.data()['title'],
-                              subtitle: sample.data()['description']);
-                        },
-                        itemCount: snapshot.data!.docs.length,
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const SizedBox(height: 10),
-                      );
-                    } else {
-                      return Center(
-                        child: Text(
-                          'Quiz not created ! 😩',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 14.0.sp, fontWeight: FontWeight.w500),
-                        ),
-                      );
-                    }
-                }
-                // return Padding(
-                //   padding: EdgeInsets.all(2.0.wp),
-                //   child: CardCustom.cardQuiz(
-                //     index: 0,
-                //     title: data.toString(),
-                //     onTap: () {
-                //       Get.to(AddQuiz());
-                //     },
-                //   ),
-                // );
-              }),
-        ));
-  }
-}
-
-class AddQuiz extends StatelessWidget {
-  const AddQuiz({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    AllquizController controller = Get.put(AllquizController());
-    var judul = TextEditingController();
-    var subjudul = TextEditingController();
+    var title = TextEditingController();
+    var description = TextEditingController();
     final formkeyAddQuiz = GlobalKey<FormState>();
+    Map<String, TextEditingController> abjad = {
+      'Judul': title,
+      'Deskripsi': description
+    };
 
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Form(
-          key: formkeyAddQuiz,
-          child: ListView(
-            padding: EdgeInsets.all(4.0.wp),
-            children: [
-              Formulir.formReguler(title: 'Judul', ctr: judul),
-              SizedBox(height: 5.0.wp),
-              Formulir.formReguler(title: 'SubJudul', ctr: subjudul),
-              SizedBox(height: 5.0.wp),
-              buttonBlueObx(
-                  l: controller.l.value,
-                  onPressed: () {
-                    if (formkeyAddQuiz.currentState!.validate()) {}
+      appBar: AppBar(title: Text('Semua Kuis - ${data?["title"]}')),
+      body: Padding(
+        padding: EdgeInsets.all(3.0.wp),
+        child: StreamBuilder(
+          stream: controller.fetchAllQuiz("${data?['uid']}"),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Center(child: Text('No Internet'));
+              case ConnectionState.waiting:
+                return ListView.separated(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey.shade200,
+                      highlightColor: Colors.cyan.shade300,
+                      child: const Card(child: SizedBox(height: 80)),
+                    );
                   },
-                  teks: 'TAMBAH')
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 10),
+                  itemCount: 3,
+                );
+              default:
+                if (snapshot.hasData) {
+                  return ListView.separated(
+                    itemBuilder: (context, index) {
+                      var sample = snapshot.data!.docs.elementAt(index);
+
+                      return CardCustom.cardQuiz(
+                          onTap: () =>
+                              Get.toNamed(Routes.PRAQUIZ, arguments: sample.id),
+                          index: index,
+                          title: sample.data()['title'],
+                          subtitle: sample.data()['description']);
+                    },
+                    itemCount: snapshot.data!.docs.length,
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const SizedBox(height: 10),
+                  );
+                } else {
+                  return Center(
+                    child: Text(
+                      'Quiz not created ! 😩',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 14.0.sp, fontWeight: FontWeight.w500),
+                    ),
+                  );
+                }
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.biruTua,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
+        onPressed: () => Get.bottomSheet(
+          CardCustom.formBottomSheet(
+            key: formkeyAddQuiz,
+            children: [
+              ...abjad.entries.map((e) => Column(
+                    children: [
+                      Formulir.formReguler(title: e.key, ctr: e.value),
+                      SizedBox(height: 5.0.wp)
+                    ],
+                  )),
+
+              // tombol
+              Obx(
+                () => buttonBlueObx(
+                    l: controller.l.value,
+                    onPressed: () {
+                      if (formkeyAddQuiz.currentState!.validate()) {
+                        controller.createQuiz(
+                            title: title.text,
+                            description: description.text,
+                            category: data?['uid']);
+                      }
+                    },
+                    teks: 'TAMBAH'),
+              ),
             ],
           ),
         ),
